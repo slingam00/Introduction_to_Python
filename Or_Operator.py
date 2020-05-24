914ce0208f0fb870df1b91cd55a19e80a09b9aee
Sanjeev Lingam-Nattamai <42702699+slingam00@users.noreply.github.com>
2020-05-24 03:13:47 -0700

    Created Or_Operator.py

---
+++ Or_Operator.py
@@ -0,0 +1,6 @@
+breakpointValue = 5
+valueOne = 1
+valueTwo = 15
+
+if valueOne > breakpointValue or valueTwo > breakpointValue:
+    print("At least one value is greater than the breakpoint value")
