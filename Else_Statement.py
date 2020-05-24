356380b67b7937ed01942142d2c86de6f1fa6df4
Sanjeev Lingam-Nattamai <42702699+slingam00@users.noreply.github.com>
2020-05-24 03:11:56 -0700

    Created Else_Statement.py

---
+++ Else_Statement.py
@@ -0,0 +1,8 @@
+temperature = 50
+
+if temperature > 65:
+    print("I will go outside")
+else:
+    print("I will not go outside")   # Since the if-statement is false, 
+                                     # the else-statement is then executed
+                                     # 'I will not go outside' is then printed
