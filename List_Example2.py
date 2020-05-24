94f6d388e4e69a48f33e2be8320592ac1143e803
Sanjeev Lingam-Nattamai <42702699+slingam00@users.noreply.github.com>
2020-05-24 14:11:17 -0700

    Created List_Example2.py

---
+++ List_Example2.py
@@ -0,0 +1,12 @@
+myList = [1, 2, 3, 4, 5]
+
+# List Appending
+myList.append(6)  
+print(myList)       # Result: [1, 2, 3, 4, 5, 6]
+
+# Removing an element
+myList.remove(2)
+print(myList)       # Result: [1, 3, 4, 5, 6]
+
+# Getting the Length of a List
+print(len(myList))  # Result: 6
