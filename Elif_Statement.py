ecaaa5fbada12f7cf6b434a43a57d7fa3f913894
Sanjeev Lingam-Nattamai <42702699+slingam00@users.noreply.github.com>
2020-05-24 03:12:30 -0700

    Created Elif_Statement.py

---
+++ Elif_Statement.py
@@ -0,0 +1,8 @@
+day = "Tuesday"
+
+if day == "Sunday":
+    print("I will go on a run today")      # first if-statement is checked and is false
+elif day == "Tuesday":                     # then elif-statement is checked and is true
+    print("I will eat tacos today")        # "I will eat tacos today" is printed
+else:                                                                                             
+    print("I will neither go on a run nor eat tacos today")
