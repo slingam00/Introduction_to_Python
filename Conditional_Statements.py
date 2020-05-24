f8faba9f73788a8eaa5c6627def1f4aaf79f2926
Sanjeev Lingam-Nattamai <42702699+slingam00@users.noreply.github.com>
2020-05-24 03:08:12 -0700

    Created Conditional_Statements.py

---
+++ Conditional_Statements.py
@@ -0,0 +1,22 @@
+# If Statements
+if True:
+  print("The If statement is True")
+ 
+if 2 > 1:
+  print("2 is greater than 1")
+ 
+
+# Else Statements
+if 2 < 1:
+  print("2 is less than 1")
+else:
+  print("2 is not less than 1")
+
+  
+# Elif Statements
+if 2 < 1:
+  print("2 is less than 1")
+elif 2 > 1:
+  print("2 is greater than 1")
+else:
+  print("2 is equal to 1")
