d4866dfb5c41b3174591b5d2e9e1bd4e804a9de2
Sanjeev Lingam-Nattamai <42702699+slingam00@users.noreply.github.com>
2020-05-24 16:33:33 -0700

    Created For_Loops.py

---
+++ For_Loops.py
@@ -0,0 +1,13 @@
+myList = [1, 2, 3, 4, 5]
+
+# Iterating through a list
+for i in myList:   
+    print(i)       # The elements in myList will each get printed on a new line
+  
+for i in range(len(myList)):
+    print(myList[i])    # Same thing, but the elements are getting index and then printed out
+    
+# Iterating through a string
+myString = "hello"
+for i in myString:
+    print(i)            # each letter printed out on a seperate line
