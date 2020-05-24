f92e1d42040d40d3912280c05548057dcba49f47
Sanjeev Lingam-Nattamai <42702699+slingam00@users.noreply.github.com>
2020-05-24 16:34:12 -0700

    Created While_Loops.py

---
+++ While_Loops.py
@@ -0,0 +1,7 @@
+incrementer = 1
+while incrementer < 6:
+    print(incrementer)
+    incrementer = incrementer + 1 # re-assigning incrementer to one value higher
+                                  # while loop breaks when incrementer equals 6
+                                  # the while loop runs 5 times and 
+                                  # 1, 2, 3, 4, and 5 are printed on 5 lines
